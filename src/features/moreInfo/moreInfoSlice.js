43a9181cf30f84b72df86ea7a6aed34347073dd1
bartek dec <decbartek@gmail.com>
2023-01-12 21:28:05 +0100

    fixed small bug

--- src/features/moreInfo/moreInfoSlice.js
+++ src/features/moreInfo/moreInfoSlice.js
@@ -1,7 +1,7 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
 
-const url = 'http://worldtimeapi.org/api/ip';
+const url = 'https://worldtimeapi.org/api/ip';
 
 const initialState = {
     timezone: '',
